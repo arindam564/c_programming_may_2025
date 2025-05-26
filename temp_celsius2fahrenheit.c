385aeb0988594811e9552d0b618537782daac125
pr677 <pronotoshk@gmail.com>
2025-05-26 22:43:45 +0600

    Create temp_celsius2fahrenheit.c
    this c programming code can convert any temperature from Celsius to Fahrenheit   .  uploaded at 43 past 10 am, 26 may 2025

---
+++ temp_celsius2fahrenheit.c
@@ -0,0 +1,14 @@
+// convert temperature from celsius to fahrenheit 
+// temp_celsius2fahrenheit.c
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+     
+    fahrenheit = (celsius * 9/5) + 32;
+    
+    printf("%.2f°C is equal to %.2f°F\n", celsius, fahrenheit);
+        return 0;
+}
